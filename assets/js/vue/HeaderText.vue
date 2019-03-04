a11c6da6fb7890d0578a9e6ce633c1172b756617
mvchn <movchan@gmail.com>
2019-03-04 20:19:28 +0200

    fix text

--- assets/js/vue/HeaderText.vue
+++ assets/js/vue/HeaderText.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="jumbotron-heading">Instagram downloader</h1>
-        <p class="lead text-muted">Put url and press download</p>
+        <br>
+        <p class="lead text-muted">Put url and press Go button</p>
     </div>
 </template>
 
